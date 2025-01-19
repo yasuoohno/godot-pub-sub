8642c5b99d661277b41ba93278f433008f69ac28
Yasuo Ohno <yasuo.ohno@gmail.com>
2025-01-19 19:01:27 +0900

    tested with Godot 4.3

--- addons/pub-sub/PubSub.gd
+++ addons/pub-sub/PubSub.gd
@@ -1,108 +1,153 @@
 """
 Publish-Subscribe mechanism
+
+Only one subscription per object per event is allowed.
+Invalid or queued-for-deletion objects will not trigger event handlers.
+
+subscribe/unsubscribe parameters: listener, event_key
+publish parameters: event_key, payload
 """
 extends Node
 class_name PubSub
 
-const subscriptions = {}
 
-const instant_subscriptions = {}
+static var subscriptions = {}
+static var instant_subscriptions = {}
+static var published_async = []
+static var all_events = []
+static var services = {}
 
-const published_async = []
+const event_handler_name = "event_published"
+const instant_event_hanlder_name = "instant_event_published"
 
-const all_events = []
 
-const services = {}
-
-static func subscribe(event_key, listener)->void:
+static func is_valid(listener)->bool:
 	"""
-	Subscribes listener to the given event_key.
+	Confirm the listener instance is valid and is not queued for deletion.
 	"""
-	if !listener.has_method("event_published"):
-		# No method to call
+	if !is_instance_valid(listener):
+		return false
+	if listener is Node:
+		return !listener.is_queued_for_deletion()
+	else:
+		return true
+
+
+static func is_a_listener(obj, method_name)->bool:
+	"""
+	Confirm the listener is valid and has event handler.
+	"""
+	return is_valid(obj) and obj.has_method(method_name)
+
+
+static func subscribe(listener, event_key = null)->void:
+	"""
+	Subscribes listener to the given event_key, or all event_keys if not supplied.
+	"""
+	if !is_a_listener(listener, event_handler_name):
+		# Invalid object or No method to call
 		return
-	if event_key=="":
+
+	if event_key==null:
 		# Subscribe to all events
 		all_events.append(listener)
-	elif subscriptions.has(event_key):
-		if !subscriptions[event_key].has(listener):
-			subscriptions[event_key].append(listener)
 	else:
-		subscriptions[event_key] = [listener]
+		# add a listener to the listener list.
+		var listeners = subscriptions.get_or_add(event_key, [])
+		# Ensure subscription once.
+		if !listeners.has(listener):
+			listeners.append(listener)
+
 
 static func unsubscribe(listener, event_key = null)->void:
 	"""
 	Unsubscribes listener from event_key, or all event_keys if not supplied
 	"""
+	#var erase_all = func(arr):
+	#	arr.filter(func(v): return v != listener)
+	# MEMO: Use this lambda function to handle multiple subscriptions per object.
+
 	if event_key==null:
-		for arr in subscriptions.values():
-			if arr.has(listener): arr.erase(listener)
-		if all_events.has(listener):
-			all_events.erase(listener)
-	else:
+		# remove all subscriptions
+		for listeners in subscriptions.values():
+			listeners.erase(listener)
+		all_events.erase(listener)
+	elif subscriptions.has(event_key):
 		subscriptions[event_key].erase(listener)
+
+	# MEMO: If a listener has been removed,
+	# this implementation will not remove it from published_async events. 
+
 
 static func publish(event_key, payload = null)->void:
 	"""
-	Publishes the given event_key and payload. Subscribers to event_key will 
-	have their eventPublished methods called.
+	Publishes the given event_key and payload.
+	Subscribers to event_key will have their event_published methods called.
 	"""
-	if !subscriptions.has(event_key): return
-	var listeners = subscriptions[event_key] as Array
-	var toRemove = []
-	for listener in listeners:
-		if listener.is_queued_for_deletion():
-			toRemove.add(listener)
-		else:
-			listener.event_published(event_key, payload)
-	# Tidy up any deleted listener objects
-	for listenerToRemove in toRemove:
-		listeners.erase(listenerToRemove)
+	var invalid_listeners = []
+	var called = []
+
+	if subscriptions.has(event_key):
+		var listeners = subscriptions[event_key]
+		for listener in listeners:
+			if is_valid(listener):
+				listener.call(event_handler_name, event_key, payload)
+				called.append(listener)
+			else:
+				invalid_listeners.append(listener)
+
+	for listener in all_events:
+		if !called.has(listener):
+			if is_valid(listener):
+				listener.call(event_handler_name, event_key, payload)
+			else:
+				invalid_listeners.append(listener)
+
+	# publish event to all valid listeners.
+
+	# unsubscribe invalid listeners
+	for invalid_listener in invalid_listeners:
+		unsubscribe(invalid_listener)
+
 
 static func publish_to_random(event_key:String, payload)->void:
 	"""
 	Publish an event to a single randomly-chosen subscriber
 	"""
-	if !subscriptions.has(event_key): return
+	if !subscriptions.has(event_key):
+		return
+
 	var listeners = subscriptions[event_key]
-	var index = rand_range(0, listeners.length-1)
-	listeners[index].eventPublished(event_key, payload)
+	if listeners.size() == 0:
+		return
+
+	var listener = listeners[randi_range(0, listeners.size()-1)]
+	if is_valid(listener):
+		listener.call(event_handler_name, event_key, payload)
+	# MEMO: Handlers may not be invoked if the listener is invalid.
 
 
-static func publish_async(event_key, payload)->void:
-	"""
-	Queue the given event for async publishing. PubSub._process() MUST be called for this to work!
-	"""
-	if !subscriptions.has(event_key): return
-	var listeners = subscriptions[event_key]
-	var toRemove = []
-	for listener in listeners:
-		if listener.is_queued_for_deletion():
-			toRemove.add(listener)
-		elif listener.has_method("event_published"):
-			published_async.push_front([listener, event_key, payload])
-	# Tidy up any deleted listener objects
-	for listenerToRemove in toRemove:
-		listeners.erase(listenerToRemove)
-	
-static func subscribe_instant(event_key, listener)->void:
+static func subscribe_instant(listener, event_key)->void:
 	"""
 	Subscribes listener to the given instant event_key.
 	"""
-	if instant_subscriptions.has(event_key):
-		if !instant_subscriptions[event_key].has(listener):
-			instant_subscriptions[event_key].append(listener)
-	else:
-		instant_subscriptions[event_key] = [listener]
+	if !is_a_listener(listener, instant_event_hanlder_name):
+		# Invalid object or No method to call
+		return
+	
+	var listeners = instant_subscriptions.get_or_add(event_key, [])
+	if !listeners.has(listener):
+		listeners.append(listener)
+
 
 static func unsubscribe_instant(listener, event_key = null)->void:
 	"""
 	Unsubscribes listener from instant event_key, or all event_keys if not supplied
 	"""
 	if event_key==null:
-		for arr in instant_subscriptions.values():
-			if arr.has(listener): arr.erase(listener)
-	else:
+		for listeners in instant_subscriptions.values():
+			listeners.erase(listener)
+	elif instant_subscriptions.has(event_key):
 		instant_subscriptions[event_key].erase(listener)
 
 
@@ -111,25 +156,66 @@ static func publish_instant(event_key, payload)->Array:
 	Publish the given instant event key to all listeners and return an array of their responses
 	"""
 	var result = []
-	if !subscriptions.has(event_key): return result
-	var listeners = subscriptions[event_key] as Array
+	var invalid_listeners = []
+
+	if !instant_subscriptions.has(event_key):
+		return result
+	var listeners = instant_subscriptions[event_key]
 	for listener in listeners:
-		result.append(listener.instantEventPublished(event_key, payload))
+		if is_valid(listener):
+			result.append(listener.call(instant_event_hanlder_name, event_key, payload))
+		else:
+			invalid_listeners.append(listener)
+
+	# unsubscribe invalid listeners
+	for invalid_listener in invalid_listeners:
+		unsubscribe_instant(invalid_listener)
+
 	return result
 
 
-static func process()->void:
+static func publish_async(event_key, payload)->void:
+	"""
+	Queue the given event for async publishing. PubSub.process() MUST be called for this to work!
+	"""
+	var calling = []
+
+	if subscriptions.has(event_key):
+		var listeners = subscriptions[event_key]
+		for listener in listeners:
+			if is_valid(listener):
+				# MEMO: published_async is a FIFO.
+				published_async.push_front([listener, event_key, payload])
+				calling.append(listener)
+
+	for listener in all_events:
+		if is_valid(listener) and !calling.has(listener):
+			published_async.push_front([listener, event_key, payload])
+
+
+static func process_async()->bool:
 	"""
 	Process the next outstanding async event, if any.
+	
+	process only one event per call.
+	return false, if there is no async event.
 	"""
-	if published_async.size()==0: return
+	if published_async.size()==0:
+		return false
 
 	var arr = published_async.pop_back() as Array
 	var listener = arr[0]
 	var event_key = arr[1]
 	var payload = arr[2]
-	if listener.has_method("event_published"):
-		listener.event_published(event_key, payload)
+	if is_valid(listener):
+		listener.call(event_handler_name, event_key, payload)
+
+	return published_async.size() > 0
+
+
+static func process_async_all()->void:
+	while process_async():
+		pass
 
 
 static func register_service(service_id:int, provider)->void:
@@ -143,7 +229,10 @@ static func get_service(service_id:int):
 	"""
 	Returns the service registered under the given int service_id
 	"""
-	return services[service_id]
+	if services.has(service_id):
+		return services[service_id]
+
+	return null
 
 
 static func clear()->void:
@@ -155,6 +244,7 @@ static func clear()->void:
 	published_async.clear()
 	all_events.clear()
 	services.clear()
+
 
 static func clear_instant_events()->void:
 	"""
